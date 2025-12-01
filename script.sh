6ba41b3fc56ed094de1cdfc87b2c9b731fb06e8a
liamgen.js <services@liamcharpentier.fr>
2025-12-01 23:01:09 +0100

    Remappage dossiers
    - Changement dossier livrables
    - Prise en charge images (en cours)

--- script.sh
+++ script.sh
@@ -8,18 +8,53 @@ docker image pull bigpapoo/sae103-html2pdf
 
 # Récupérer tous les excel
 
-nbFichierExcel=$(ls *.xlsx | wc -l)
+nbFichierExcel=$(ls fichiers/*.xlsx | wc -l)
 if [ "$nbFichierExcel" -gt 0 ]
 then
     # Lancer container docker
 
     docker run -dit --name excel2csv bigpapoo/sae103-excel2csv bash
 
+    for chemin in fichiers/*.xlsx
+    do
+        
+        if [ -f "$chemin" ]
+        then
+            # Récupérer fichier sans dossier
+            nomFichier="$(basename "$chemin")"
+            # Récupérer fichier avec extension csv
+            nomFichierCsv="${nomFichier%.xlsx}.csv"
+
+            docker container cp "$chemin" excel2csv:"/app/$nomFichier"
+            docker container exec -it excel2csv ssconvert "$nomFichier" "$nomFichierCsv"
+            docker container cp excel2csv:"/app/$nomFichierCsv" utilisables/"$nomFichierCsv"
+        fi
+    done
+
+    docker container stop excel2csv
+    docker container rm excel2csv
+fi
+
+nbFichierImg=$(ls fichiers/*.png fichiers/*.jpeg fichiers/*.jpg fichiers/*.webp | wc -l)
+if [ "$nbFichierImg" -gt 0 ]
+then
+    # Lancer container docker
+
+    docker run -dit --name imagick bigpapoo/sae103-imagick bash
+
     for fichier in *.xlsx
     do
         if [ -f "$fichier" ]
         then
-            echo "$fichier"
+            # Récupérer fichier sans dossier
+            nomFichier="$(basename "$chemin")"
+
+            docker container cp "$chemin" excel2csv:"/app/$nomFichier"
+            docker container exec -it excel2csv ssconvert "$fichier" "$nomFichierCsv"
+            docker container cp excel2csv:"/app/$nomFichierCsv" "$nomFichierCsv"
         fi
     done
+
+    docker container stop excel2csv
+    docker container rm excel2csv
 fi
